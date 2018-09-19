82393612bca9a79046b66104758f4f46e94ad350
jubel <hanbingxin49@gmail.com>
2018-09-19 17:18:18 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# serverless-event-body-option
-The serverless plugin that provides the extra CLI option for the invoke command to support passing the HTTP body data.
+[![serverless](http://public.serverless.com/badges/v3.svg)](http://www.serverless.com)
+
+# Serverless Event Body Option
+
+## Overview
+
+This is a plugin of the [serverless framework](https://github.com/serverless/serverless), it provides the extra CLI option `--body/-b` for the invoke (local) command 
+to support passing the HTTP body data directly.
+
+
+Currently, The serverless framework providing the `--data` and `--path` options for the invoke command to passing the event data to the handler functions.
+
+However, we have to use the string formatted(instead of JSON) HTTP body data for the value of `body` field in the JSON formatted event data.
+
+which means that we have to escape the quotes for the actually json based body data.
+
+e.g:
+```json
+{"body": "{\"body_key\": \"body_value\"}"}
+```
+
+That makes the body data unreadable and unmanageable, especially for the complicated body data.   
+
+This plugin helped that! 
+with it, we can specify the JSON formatted HTTP body data in the CLI directly instead of wrapping them in the event.
+
+
+## Installation
+
+1. Performing `npm install` at the root path of your each service.
+
+```
+$ npm install --save serverless-event-body-option
+```
+
+2. Adding the plugin to your `serverless.yml` file.
+
+
+```yaml
+plugins:
+  - serverless-event-body-option
+```
+
+
+## Usage
+
+### Invoking the function
+
+* Specifying the data from stdin directly if the data is short.
+
+```
+$ serverless invoke -f yourFunction --body '{"foo":"var"}'
+```
+
+* Specifying json file path which contains the body data if the data is complicated.
+
+```
+$ serverless invoke -f yourFunction --body /path/to/your-body-data.json
+```
+
+You can also adding the body option for the `invoke local` sub-command for invoking the function locally. 
---
+++ index.js
@@ -0,0 +1,82 @@
+'use strict';
+const  BbPromise = require('bluebird');
+
+class BodyOption {
+  constructor(serverless, options) {
+    this.serverless = serverless;
+    this.options = options;
+
+    this.commands = {
+      invoke: {
+        lifecycleEvents: [
+          "invoke",
+        ],
+
+        options: {
+          body: {
+            usage: 'The HTTP body data in the event, format: Json string or json file path.',
+            shortcut: 'd',
+          },
+        },
+
+        commands: {
+          local: {
+            lifecycleEvents: [
+              "invoke",
+            ],
+
+            options: {
+              body: {
+                usage: 'The HTTP body data in the event, format: Json string or json file path.',
+                shortcut: 'd',
+              },
+            },
+          },
+        },
+      },
+    };
+
+    this.hooks = {
+      'before:invoke:local:invoke': this.handleBodyOption.bind(this),
+      'before:invoke:invoke': this.handleBodyOption.bind(this),
+    }
+  }
+
+  handleBodyOption() {
+    if (this.options.body !== undefined) {
+      const bodyOption = this.options.body;
+      const bodyJson = (this.isFilePath(bodyOption))
+        ? this.getBodyFromPath(bodyOption)
+        : this.getDataFromCLI(bodyOption);
+
+      this.wrapBodyToEvent(bodyJson);
+      return BbPromise.resolve(this);
+    }
+  }
+
+  wrapBodyToEvent(body) {
+    this.options.data = {'body': JSON.stringify(body).trim() };
+  }
+
+  isFilePath(arg) {
+    return this.serverless.utils.fileExistsSync(arg);
+  }
+
+  getBodyFromPath(path) {
+    this.serverless.cli.log(`Getting body data from path: ${path}`);
+    return this.serverless.utils.readFileSync(path);
+  }
+
+  getDataFromCLI(body) {
+    this.serverless.cli.log(`Getting body data from stdin: ${body}`);
+    let data;
+    try {
+      data = JSON.parse(body);
+    } catch(e) {
+      data = "(" + body + ")";
+    }
+    return data;
+  }
+}
+
+module.exports = BodyOption;
